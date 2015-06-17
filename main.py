de002aa0ed1964b0b0fc6995ac007b991a2e646d
JD <noideal@users.noreply.github.com>
2015-06-17 12:05:07 -0600

    main.argv
    Added command line functionality.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import sys
 import threading
 from queue import Queue
 import time
 import btc_connect
 
 ## Draft - Will need to change a lot of this :)
-# Add: exceptions, loop breaks, csv reader for timer/exchange values, ...
+# Add: exceptions, loop breaks, csv reader for timer values, ...
 
 print_lock = threading.Lock()
 
@@ -14,7 +15,7 @@ def job(worker):
  if worker=='bitfinex':t_bitfinex()
  if worker=='btcchina':t_btcchina()
 
- time.sleep(0.5)
+ time.sleep(0.1)
  with print_lock:
   print(threading.current_thread().name,worker)
 
@@ -39,22 +40,23 @@ def threader():
   job(worker)
   q.task_done()
 
+def main(args):
+
+	start = time.time() # start threads
+
+	q.put('btce')
+	q.put('bitfinex')
+	q.put('btcchina')
+
+	q.join() # at thread terminate
+
+	print('Time:',time.time()-start)
+
 q = Queue()
 for x in range(5):
  t = threading.Thread(target = threader)
  t.daemon = True
  t.start()
-
-# start threads
-start = time.time()
-## iterate through dict/list exchanges(timer)
-#for worker in range(20):
-# q.put(worker)
-
-q.put('btce')
-q.put('bitfinex')
-q.put('btcchina')
-
-q.join() # at thread terminate
-
-print('Time:',time.time()-start)
+	 
+if __name__ == '__main__':
+    main(sys.argv[1:])
